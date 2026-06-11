b0528b748a08f388c109e18219439cc2481713a8
MukeshDixena <mukesh.dixena2002@gmail.com>
2026-06-11 16:07:38 +0530

    chore: update base path and remote for birthday-magic repo

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/happybirthday/',
+  base: '/birthday-magic/',
   build: {
     chunkSizeWarningLimit: 1200,
     rollupOptions: {
